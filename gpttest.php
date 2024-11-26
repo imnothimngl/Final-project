5ff954f66de4634b6653e2ff305a6d0cf07642a9
imnothimngl <jaymathewcdael@su.edu.ph>
2024-11-26 10:36:00 +0800

    final proj

--- gpttest.php
+++ gpttest.php
@@ -1,20 +1,31 @@
-<?php
-// You can define your background image in the PHP file if needed
-$profpic = "bgduma.jpg"; // Replace with the correct image path
-?>
 
 <!DOCTYPE html>
-<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hello with Background Image</title>
-    <!-- Link to the external CSS file -->
-    <link rel="stylesheet" type="text/css" href="style.css">
+	 <style>
+        input {
+            border-radius: 15px;
+            padding: 10px;
+            border: 2px solid #ccc;
+            width: 250px;
+        }
+    </style>
+    <title>LoginPage</title>
+    <link rel="stylesheet" type="text/css" href="teststyel.css">
 </head>
 <body>
-    <div class="content">
-        <h1>Hello!</h1>
+    <div id="content">
+		<form method="post" action="!!!!!!!!">
+        <h1> Sign In</h1>
+			Username:<br>
+			<input type="text"	name="name"><br><br>
+			Password:<br>
+			
+			<input type="Password"	name="name"><br><br>
+							 
+			<input type="submit"	name="submit"><br><br>
+			</form>
     </div>
 </body>
 </html>
